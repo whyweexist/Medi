84013c5f1b9401944b733efdeff911b72b3bb02d
whyweexist <167321903+whyweexist@users.noreply.github.com>
2024-09-25 23:48:48 +0530

    Update category-search.tsx

--- src/components/category-search.tsx
+++ src/components/category-search.tsx
@@ -95,28 +95,20 @@ function CategorySearch() {
       </div>
 
       {/* Display List of Category  */}
-      <div className="grid grid-cols-3 mt-5 md:grid-cols-4 lg:grid-cols-6 ">
-        {categoryList.length > 0
-          ? categoryList.map(
-              (item, index) =>
-                index < 6 && (
-                  <Link
-                    href={"/search/" + item.Name}
-                    key={index}
-                    className="flex 
-            flex-col text-center items-center
-            p-5 bg-blue-50 m-2 rounded-lg cursor-pointer
-            gap-2 hover:scale-110 transition-all ease-in-out"
-                  >
-                    <Image src={item.url} alt="icon" width={40} height={40} />
-                    <label className="text-blue-600 text-sm">{item.Name}</label>
-                  </Link>
-                )
-            )
-          : [1, 2, 3, 4, 5, 6].map((item, index) => (
-              <div className=" bg-slate-200 m-2 w-[130px] h-[120px] rounded-lg animate-pulse"></div>
-            ))}
+       <div className="grid grid-cols-3 gap-2 border rounded-lg p-5">
+        {timeSlot?.map((item, index) => (
+          <h2
+            key={`${item.time}-${index}`} // Unique key here
+            onClick={() => setSelectedTimeSlot(item.time)}
+            className={`p-2 border cursor-pointer text-center hover:bg-primary hover:text-white rounded-full ${
+              item.time === selectedTimeSlot && "bg-primary text-white"
+            }`}
+          >
+            {item.time}
+          </h2>
+        ))}
       </div>
+
     </div>
   );
 }
